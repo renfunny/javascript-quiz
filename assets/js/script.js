6bd40c6b6e26c7a878567268f72f63c6c80a915f
Renato Cordova <ren99cordova@gmail.com>
2022-09-26 21:51:40 -0400

    added more comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Questions
+//Questions array
 var questions = [
   {
     question: `Which of the following is NOT a data type?`,
@@ -65,24 +65,33 @@ var questions = [
     answer: `optionA`,
   },
 ];
-
+// score will be used for the timer
 var score = 75;
+//number of highscores allowed
 var numHs = 5;
+//highscore variables
 var highScore = `highScores`;
 var highScores = JSON.parse(localStorage.getItem(highScores)) ?? [];
 var highScoreString = localStorage.getItem(highScore);
+//used for timer, made it a global var so i can clear the interval in other functions
 var timeInterval;
+//dom elements
 var timeEl = document.querySelector(".time");
 var quizMenu = document.querySelector(`.main-menu`);
 var quizQuestions = document.querySelector(`.quiz-questions`);
+//will be used as an indicator of the questions array index
 var questionNumber = 0;
+//////////////////////////////////////////////////////////////////////
+//Functions: I numbered them according to the order
 
+// clears local storage and the dom element holding highscores
 function clearHighScores() {
   localStorage.clear(`highScores`);
   var highScoreList = document.querySelector(`.high-scores`);
   highScoreList.innerHTML = "";
 }
 
+// allows us to go back to the menu and reset everything
 function backToMenu() {
   document.querySelector(`.highscore-list`).classList.add(`hidden`);
   document.querySelector(`.highscore-menu`).classList.add(`hidden`);
@@ -95,7 +104,9 @@ function backToMenu() {
   document.querySelector(`.result`).textContent = "";
 }
 
+//8. clears interval incase its called during the quiz, and adds the highscores sored in the local storage to the dom
 function displayHighScores() {
+  clearInterval(timeInterval);
   var highScoreList = document.querySelector(`.high-scores`);
   highScoreList.innerHTML = highScores
     .map((score) => `<li>${score.name} - ${score.score}</li>`)
@@ -107,6 +118,7 @@ function displayHighScores() {
   quizQuestions.classList.add("hidden");
 }
 
+//7. grabs the initials input and saves it with the score in an object, it sorts is based on highest to lowest score and gets rid of any scores that exceed the number of highscores allowed to be saved
 function saveHighScore(score, highScores) {
   var name = document.querySelector(`#initials-input`).value;
   var newScore = { score, name };
@@ -118,37 +130,39 @@ function saveHighScore(score, highScores) {
   displayHighScores();
 }
 
+//6. makes sure the score is not negative and calls saveHighScore so it can be saved
 function checkHighScore() {
   if (score < 0) score = 0;
   saveHighScore(score, highScores);
 }
 
-//Displays hishscore menu and adds event to submit button
+//5. Displays hishscore menu and adds event to submit button
 var showHighscoreMenu = function () {
   clearInterval(timeInterval);
   quizQuestions.classList.add(`hidden`);
   document.querySelector(`.highscore-menu`).classList.remove(`hidden`);
-  document.querySelector(`.final-score`).textContent = score;
-
+  //allows it to not display negative values
+  if (score >= 0) {
+    document.querySelector(`.final-score`).textContent = score;
+  } else {
+    document.querySelector(`.final-score`).textContent = `0`;
+  }
   var submitBtn = document.querySelector(`.submit-btn`);
   submitBtn.addEventListener(`click`, checkHighScore);
-
+  //resets the question number, the timer and the result element
   questionNumber = 0;
   timeEl.textContent = 0;
   document.querySelector(`.result`).textContent = "";
 };
 
-//Starts the timer and calls for displayQuiz function once
+// 1. Starts the timer and calls for displayQuiz function
 function startQuiz() {
   //Starts timer
   setTimer();
-  var done = false;
-  if (!done) {
-    done = true;
-    displayQuiz(questionNumber);
-  }
+  displayQuiz(questionNumber);
 }
 
+// 2.A starts timer and updates the time element, if score is 0 or below (negative) it will make the score = 0 to overwrite any negative values and send you to the end of the quiz
 function setTimer() {
   timeInterval = setInterval(function () {
     score--;
@@ -161,7 +175,7 @@ function setTimer() {
   }, 1000);
 }
 
-//Displays the current question from the questions array, starting at index 0
+// 2.B Displays the current question from the questions array, starting at index 0
 var displayQuiz = function (questionNumber) {
   //Hides the main menu
   quizMenu.classList.add(`hidden`);
@@ -176,7 +190,7 @@ var displayQuiz = function (questionNumber) {
   evaluateAnswer();
 };
 
-//checks if the clicked option has the "correct" data-state
+//4. Checks if the clicked option has the "correct" data-state, if so it adds 1 to the questionNumber which is the index of the questions array, displays correct, switches the data-state back to `incorrect`, and if theres another question it will call displayQuiz once more with the updated index (therefore showing the next question). If the data-state is `incorrect` the same will happen but it will deduct 10 from the score/timer. This will loop until there are no questions left
 var checkAnswer = function (event) {
   var userInput = event.target;
   var currentQuestion = questions[questionNumber];
@@ -211,7 +225,7 @@ var checkAnswer = function (event) {
   }
 };
 
-//Evaluates which option of the current question is the right answer and changes the data-state to "correct" while the rest remain "incorrect" then adds the click event to the options which will then trigger the checkAnswer function when clicked
+//3. Evaluates which option of the current question is the right answer and changes the data-state to "correct" while the rest remain "incorrect" then adds the click event to the options which will then trigger the checkAnswer function when clicked
 var evaluateAnswer = function () {
   var currentQuestion = questions[questionNumber];
   var currentAnswer = currentQuestion.answer;
@@ -223,6 +237,7 @@ var evaluateAnswer = function () {
   optionContainer.addEventListener(`click`, checkAnswer);
 };
 
+//click events
 document.querySelector(`.start-btn`).addEventListener(`click`, startQuiz);
 
 document
